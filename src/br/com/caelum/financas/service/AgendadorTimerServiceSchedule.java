4d7b421b08e30fa9be88420d114e5f4fed121397
alexandregama <alexandre.gama.lima@gmail.com>
2015-06-26 22:35:46 -0300

    Cria um Agendador usando @Schedule do EJB 3.1

---
+++ src/br/com/caelum/financas/service/AgendadorTimerServiceSchedule.java
@@ -0,0 +1,28 @@
+package br.com.caelum.financas.service;
+
+import javax.annotation.PreDestroy;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.Timer;
+
+@Singleton
+@Startup
+public class AgendadorTimerServiceSchedule {
+	
+	public AgendadorTimerServiceSchedule() {
+		System.out.println("Construindo o AgendadorTimerServiceSchedule");
+	}
+	
+	@Schedule(hour = "*", minute = "*/1", second = "0")
+	public void executa(Timer timer) {
+		System.out.println("Executando o AgendadorTimerServiceSchedule usando Schedule");
+		System.out.println("Timer: " + timer);
+	}
+	
+	@PreDestroy
+	public void preDestruicao() {
+		System.out.println("Destruindo o AgendadorTimerServiceSchedule");
+	}
+
+}
