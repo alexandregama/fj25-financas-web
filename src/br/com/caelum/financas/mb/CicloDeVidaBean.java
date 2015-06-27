d16519804f4afaf3355dde3a9233ec78ea7e3881
alexandregama <alexandre.gama.lima@gmail.com>
2015-06-26 21:24:03 -0300

    Adiciona Pool de objetos para o Agendador

--- src/br/com/caelum/financas/mb/CicloDeVidaBean.java
+++ src/br/com/caelum/financas/mb/CicloDeVidaBean.java
@@ -1,15 +1,20 @@
 package br.com.caelum.financas.mb;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import br.com.caelum.financas.service.Agendador;
 
 @Named
 @RequestScoped
 public class CicloDeVidaBean {
 	
+	@Inject
+	private Agendador agendador;
 	
 	public void executeAgendador() {
-
-	
+		agendador.executa();
 	}
+	
 }
--- src/br/com/caelum/financas/service/Agendador.java
+++ src/br/com/caelum/financas/service/Agendador.java
@@ -1,5 +1,13 @@
 package br.com.caelum.financas.service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Stateless;
+
+import org.jboss.ejb3.annotation.Pool;
+
+@Stateless
+@Pool(value = "slsb-strict-max-pool")
 public class Agendador {
 
 	private static int totalCriado;
@@ -13,6 +21,17 @@ public class Agendador {
 			Thread.sleep(4000);
 		} catch (InterruptedException e) {
 		}
+	}
+	
+	@PostConstruct
+	public void posConstrucao() {
+		System.out.println("Criando o Agendador");
+		totalCriado++;
+	}
+	
+	@PreDestroy
+	public void preDestruicao() {
+		System.out.println("Destruindo o Agendador");
 	}
 
 }
