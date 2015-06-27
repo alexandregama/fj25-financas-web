affe92e5952a7ceadbbbc171818b21ef72fe16d1
alexandregama <alexandre.gama.lima@gmail.com>
2015-06-26 22:50:01 -0300

    Adiciona mensagem indicando que o agendamento foi realizado com sucesso

--- src/br/com/caelum/financas/mb/AgendadorFormBean.java
+++ src/br/com/caelum/financas/mb/AgendadorFormBean.java
@@ -1,6 +1,8 @@
 package br.com.caelum.financas.mb;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -19,6 +21,8 @@ public class AgendadorFormBean {
 	
 	public void agendar() {
 		agendador.agenda(expressaoMinutos, expressaoSegundos);
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.addMessage(null, new FacesMessage("Agendamento registrado com sucesso"));
 	}
 
 	public String getExpressaoMinutos() {
